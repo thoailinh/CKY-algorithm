2db22aac10d5018a54fc721593ddcd2c1cc62b76
ku_mingo <34635185+thoailinh@users.noreply.github.com>
2019-05-26 10:52:01 +0700

    Add files via upload

--- cky.py
+++ cky.py
@@ -1,10 +1,101 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+# import numpy as np
+# from greader import *
+# import time
+# ###############################################################
+# #### CKY ALGORITHM
+# ###############################################################
+# #Pseudocode from wikipedia
+# def cky(Roots, NT, T, GT, GNT, words, show_table= True, gettree=False):
+#     #The input string s consist of n letters, a1... an.
+#     # print(words) #['nam', 'hoc', 'bai', 'o', 'truong']
+#     # print(type(words))
+#     n = len(words)
+#     #The grammar contain r terminal and nonterminal symbols R1... Rr.
+#     # print(NT, type(NT)) # (['VB', 'PP', 'VP', 'S', 'IN', 'NP'], <type 'list'>)
+#     r = len(NT)
+#     #Let P[n,n,r] be an array of booleans. Initialize all elements of P to false.
+#     P = np.zeros((n,n,r))
+
+
+#     #For each i = 1 to n
+#     for i in range(n):
+#         # For each unit production Rj -> ai, set P[i,1,j] = true.
+#         for R,A in GT.iteritems():
+#             w = words[i]
+#             for a in A:
+#                 if words[i] == a[0]:
+#                     P[0, i, NT.index(R)] = 1
+
+    
+#     #For each i = 2 to n -- Length of span
+#     for i in range(2, n+2):
+#         #For each j = 1 to n-i+1 -- Start of span
+#         for j in range(1, n-i+2):
+#             #For each k = 1 to i-1 -- Partition of span
+#             for k in range(1, i):
+#                 #For each production RA -> RB RC
+#                 for l,R in GNT.iteritems():
+#                     for r in R:
+#                         #if P[k,j,B] and P[i-k,j+k,C] then set P[i,j,A] = true
+#                         if P[k-1, j-1, NT.index(r[0])] == 1 and P[i-k-1,j+k-1, NT.index(r[1])] == 1:
+#                             P[i-1,j-1,NT.index(l)] = 1
+
+#     # if any of P[n,1,x] is true (x is iterated over the set s, where s are all the indices for Rs) then
+#     #   Then string is member of language
+#     #   Else string is not member of language
+#     if show_table: 
+#         display_table(P, T, NT, words)
+
+#     for r in Roots:
+#         j = NT.index(r)
+#         if P[n-1,0,j] == 1: return (True, P)
+
+#     return (False, P)
+
+
+# def display_table(P, T, NT, words):
+#     d = P.shape # (5,5,6)
+#     ##calculate the padding and the cells content
+#     cells = []
+
+#     for i in range(d[0]):
+#         for j in range(0, d[1]):
+#             c = []
+#             for k in range(0, d[2]):
+#                 if P[i,j, k] != 0:
+#                     c.append(NT[k])
+#             if len(c) != 0:
+#                 cells.append('|'.join(c))
+#             else:
+#                 cells.append('             ')
+
+#     l = np.array(cells).reshape(d[0],d[0])
+
+#     #print the table
+#     for i in range(d[0]):
+#         for j in range(d[0]):
+#             if len(l[i,j]) < 10 :
+#                 # m = str(i)
+#                 # n = str(j+1)
+#                 s = str(i) + str(j+1)
+#                 l[i,j] = l[i,j] + '|' + str(i) + ',' + str(j+1)
+
+#     print ('\n0  ' + ''.join([("%-*s" % (16, i)) for i in words]))
+#     for i in range(d[0]):
+#         print ("%d " % (i)),
+#         print (''.join([("%-*s" % (15, l[i,j])) for j in range(d[0]) ]))
+
+# R, NT, T, Gt, GT, valid = parse_cnf(open("/home/wolf/Downloads/pyCKY-master/G1.txt"))
+# string = "Nam có nhiều bạn ở trường"
+
+# list(string.split(" "))
+# cky(R, NT, T, Gt, GT, list(string.split(" ")))
+
 import numpy as np
 from greader import *
 import time
-from treelib import Node, Tree
-
 ###############################################################
 #### CKY ALGORITHM
 ###############################################################
@@ -24,7 +115,7 @@ def cky(Roots, NT, T, GT, GNT, words, show_table= True, gettree=False):
     #For each i = 1 to n
     for i in range(n):
         # For each unit production Rj -> ai, set P[i,1,j] = true.
-        for R,A in GT.iteritems():
+        for R,A in GT.items():
             w = words[i]
             for a in A:
                 if words[i] == a[0]:
@@ -38,12 +129,11 @@ def cky(Roots, NT, T, GT, GNT, words, show_table= True, gettree=False):
             #For each k = 1 to i-1 -- Partition of span
             for k in range(1, i):
                 #For each production RA -> RB RC
-                for l,R in GNT.iteritems():
+                for l,R in GNT.items():
                     for r in R:
                         #if P[k,j,B] and P[i-k,j+k,C] then set P[i,j,A] = true
                         if P[k-1, j-1, NT.index(r[0])] == 1 and P[i-k-1,j+k-1, NT.index(r[1])] == 1:
                             P[i-1,j-1,NT.index(l)] = 1
-    # print(P)
     # if any of P[n,1,x] is true (x is iterated over the set s, where s are all the indices for Rs) then
     #   Then string is member of language
     #   Else string is not member of language
@@ -58,7 +148,7 @@ def cky(Roots, NT, T, GT, GNT, words, show_table= True, gettree=False):
 
 
 def display_table(P, T, NT, words):
-    d = P.shape # (5,5,6)
+    d = P.shape 
     ##calculate the padding and the cells content
     cells = []
 
@@ -79,15 +169,13 @@ def display_table(P, T, NT, words):
     for i in range(d[0]):
         for j in range(d[0]):
             if len(l[i,j]) < 10 :
-                # m = str(i)
-                # n = str(j+1)
                 s = str(i) + str(j+1)
                 l[i,j] = l[i,j] + '|' + str(i) + ',' + str(j+1)
 
-    print '\n0  ' + ''.join([("%-*s" % (15, i)) for i in words])
+    print(('\n0  ' + ''.join([("%-*s" % (16, i)) for i in words])))
     for i in range(d[0]):
-        print "%d " % (i),
-        print (''.join([("%-*s" % (15, l[i,j])) for j in range(d[0]) ]))
+        print(("%d " % (i)), end=' ')
+        print((''.join([("%-*s" % (15, l[i,j])) for j in range(d[0]) ])))
 
     # padding = max([len(c) for c in cells]) + 2
 
@@ -101,7 +189,7 @@ def display_table(P, T, NT, words):
 
 
 R, NT, T, Gt, GT, valid = parse_cnf(open("/home/wolf/Downloads/pyCKY-master/G1.txt"))
-string = "mot loai ca moi moi xuathien o bien"
+string = "Nam có nhiều bạn ở trường"
 
 list(string.split(" "))
 cky(R, NT, T, Gt, GT, list(string.split(" ")))
--- greader.py
+++ greader.py
@@ -1,21 +1,111 @@
+# ###############################################################
+# #### SIMPLE GRAMMAR PARSING
+# ###############################################################
+# def parse_cnf(file):
+#     #lines = [line.strip() for line in file]
+#     lines = []
+#     for line in file:
+#         lines.append(line.strip())
+#     G = {}
+#     valid = True
+
+#     R = filter(None, lines[0].split(" "))
+#     for line in lines[1:]:
+#         if line.strip() != "":
+#             l, r, valid = parse_rule(line)
+
+#             if valid == True:
+#                 if not l in G.keys():
+#                     G[l] = []
+#                 G[l] = G[l] + r
+
+#     Gt = filter_dict(G, 1)
+#     GT = filter_dict(G, 2)
+
+#     #check if we have rules for the start symbols
+#     for r in R:
+#         if not r in GT.keys():
+#             print ("Grammar invalid. Not found rule for the start symbol '%s'"%r)
+#             valid = False
+
+
+#     return (R, G.keys(), find_terminals(G), Gt, GT, valid)
+
+# def filter_dict(d, l):
+#     r = {}
+#     for k, values in d.iteritems():
+#         r[k] = []
+#         for v in values:
+#             if len(v) == l: 
+#                 r[k] = r[k] + [v]
+
+#         if len(r[k]) == 0: 
+#             del r[k]
+
+#     return r
+
+# def parse_rule(line):
+#     #rule = [x.strip() for x in line.split("->")]
+#     rule = []
+#     for x in line.split("->"):
+#         rule.append(x.strip())
+#     #print line
+#     if len(rule) != 2:
+#         print ("Rule no valid: " + line)
+#         l = None
+#         r = None
+#         valid = False
+#     else:
+#         l, r = rule
+#         r = filter(None, [x.strip().split(' ') for x in r.split(" | ")])
+#         valid = validate_rule(l, r, line)
+
+#     return l, r, valid
+
+# def validate_rule(l, r, rule):
+#     invalid = False
+#     if not l.isupper():
+#         cause = "left side must be a non terminal"
+#         invalid = True
+
+#     if len(r) == 0:
+#         cause = "right side can't be empty"
+#         invalid = True
+
+#     if invalid == True:
+#         print ("Rule no valid: %s (%s)"%(rule, cause))
+
+#     return not invalid
+
+
+# def find_terminals(G):
+#     t = []
+#     for nt in G.keys():
+#         for r in G[nt]:
+#             for s in r:
+#                 if not s in G.keys() and not s in t:
+#                     t = t + [s]
+
+#     return t
+
 ###############################################################
 #### SIMPLE GRAMMAR PARSING
 ###############################################################
 def parse_cnf(file):
-    lines = [line.strip() for line in file]
-#     lines = []
-#     for line in file:
-#         lines.append(line.strip())
+    #lines = [line.strip() for line in file]
+    lines = []
+    for line in file:
+        lines.append(line.strip())
     G = {}
     valid = True
 
-    R = filter(None, lines[0].split(" "))
+    R = [_f for _f in lines[0].split(" ") if _f]
     for line in lines[1:]:
         if line.strip() != "":
             l, r, valid = parse_rule(line)
 
             if valid == True:
-                if not l in G.keys():
+                if not l in list(G.keys()):
                     G[l] = []
                 G[l] = G[l] + r
 
@@ -24,16 +114,16 @@ def parse_cnf(file):
 
     #check if we have rules for the start symbols
     for r in R:
-        if not r in GT.keys():
-            print ("Grammar invalid. Not found rule for the start symbol '%s'"%r)
+        if not r in list(GT.keys()):
+            print(("Grammar invalid. Not found rule for the start symbol '%s'"%r))
             valid = False
 
 
-    return (R, G.keys(), find_terminals(G), Gt, GT, valid)
+    return (R, list(G.keys()), find_terminals(G), Gt, GT, valid)
 
 def filter_dict(d, l):
     r = {}
-    for k, values in d.iteritems():
+    for k, values in d.items():
         r[k] = []
         for v in values:
             if len(v) == l: 
@@ -45,19 +135,19 @@ def filter_dict(d, l):
     return r
 
 def parse_rule(line):
-    rule = [x.strip() for x in line.split("->")]
-#     rule = []
-#     for x in line.split("->"):
-#         rule.append(x.strip())
+    #rule = [x.strip() for x in line.split("->")]
+    rule = []
+    for x in line.split("->"):
+        rule.append(x.strip())
     #print line
     if len(rule) != 2:
-        print ("Rule no valid: " + line)
+        print(("Rule no valid: " + line))
         l = None
         r = None
         valid = False
     else:
         l, r = rule
-        r = filter(None, [x.strip().split(' ') for x in r.split(" | ")])
+        r = [_f for _f in [x.strip().split(' ') for x in r.split(" | ")] if _f]
         valid = validate_rule(l, r, line)
 
     return l, r, valid
@@ -73,27 +163,17 @@ def validate_rule(l, r, rule):
         invalid = True
 
     if invalid == True:
-        print ("Rule no valid: %s (%s)"%(rule, cause))
+        print(("Rule no valid: %s (%s)"%(rule, cause)))
 
     return not invalid
 
 
 def find_terminals(G):
     t = []
-    for nt in G.keys():
+    for nt in list(G.keys()):
         for r in G[nt]:
             for s in r:
-                if not s in G.keys() and not s in t:
+                if not s in list(G.keys()) and not s in t:
                     t = t + [s]
 
     return t
-
-# print(parse_cnf(open("/home/wolf/Downloads/pyCKY-master/G1.txt")))
-
-# R = ['S'], 
-# G.keys() = ['VB', 'PP', 'VP', 'S', 'IN', 'NP'], 
-# G.values() = [[['hoc']], [['IN', 'NP']], [['VP', 'PP'], ['VB', 'NP'], ['hoc']], [['NP', 'VP']], [['o']], [['nam'], ['bai'], ['truong']]], 
-# find_terminals(G) = ['hoc', 'o', 'nam', 'bai', 'truong'], 
-# Gt = {'VP': [['hoc']], 'NP': [['nam'], ['bai'], ['truong']], 'VB': [['hoc']], 'IN': [['o']]}, 
-# GT = {'VP': [['VP', 'PP'], ['VB', 'NP']], 'PP': [['IN', 'NP']], 'S': [['NP', 'VP']]}, 
-# valid = True
